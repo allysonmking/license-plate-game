4f378bec385bec04e97575e1639a64267cf38420
Allyson King <50351384+allysonmking@users.noreply.github.com>
2020-12-10 18:24:44 -0600

    aws rekognition works on images in s3

---
+++ license-plate-game/app.py
@@ -0,0 +1,33 @@
+import boto3
+from chalice import Chalice
+
+app = Chalice(app_name='license-plate-game')
+
+def detect_text(photo, bucket):
+
+    client=boto3.client('rekognition')
+
+    response=client.detect_text(Image={'S3Object':{'Bucket':bucket,'Name':photo}})
+                        
+    textDetections=response['TextDetections']
+    print ('Detected text\n----------')
+    for text in textDetections:
+            print ('Detected text:' + text['DetectedText'])
+            print ('Confidence: ' + "{:.2f}".format(text['Confidence']) + "%")
+            print ('Id: {}'.format(text['Id']))
+            if 'ParentId' in text:
+                print ('Parent Id: {}'.format(text['ParentId']))
+            print ('Type:' + text['Type'])
+            print()
+    return len(textDetections)
+
+def main():
+
+    bucket='BUCKET'
+    photo='PHOTO.jpg'
+    text_count=detect_text(photo,bucket)
+    print("Text detected: " + str(text_count))
+
+
+if __name__ == "__main__":
+    main()
